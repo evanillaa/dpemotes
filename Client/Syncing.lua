bb1c2c17298820e0d0178303ae5146170d0c2acc
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-20 10:30:24 +0300

    optimze

--- Client/Syncing.lua
+++ Client/Syncing.lua
@@ -67,6 +67,7 @@ end)
 RegisterNetEvent("ClientEmoteRequestReceive")
 AddEventHandler("ClientEmoteRequestReceive", function(emotename, etype)
     isRequestAnim = true
+    requestingAnim()
     requestedemote = emotename
 
     if etype == 'Dances' then
@@ -79,30 +80,31 @@ AddEventHandler("ClientEmoteRequestReceive", function(emotename, etype)
     SimpleNotify(Config.Languages[lang]['doyouwanna']..remote.."~w~)")
 end)
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(5)
-        if IsControlJustPressed(1, 246) and isRequestAnim then
-        target, distance = GetClosestPlayer()
-            if(distance ~= -1 and distance < 3) then
-                if DP.Shared[requestedemote] ~= nil then
-                    _,_,_,otheremote = table.unpack(DP.Shared[requestedemote])
-                elseif DP.Dances[requestedemote] ~= nil then
-                    _,_,_,otheremote = table.unpack(DP.Dances[requestedemote])
+function requestingAnim()
+    Citizen.CreateThread(function()
+        while isRequestAnim do
+            Citizen.Wait(0)
+            if IsControlJustPressed(1, 246) and isRequestAnim then
+            target, distance = GetClosestPlayer()
+                if(distance ~= -1 and distance < 3) then
+                    if DP.Shared[requestedemote] ~= nil then
+                        _,_,_,otheremote = table.unpack(DP.Shared[requestedemote])
+                    elseif DP.Dances[requestedemote] ~= nil then
+                        _,_,_,otheremote = table.unpack(DP.Dances[requestedemote])
+                    end
+                    if otheremote == nil then otheremote = requestedemote end
+                    TriggerServerEvent("ServerValidEmote", GetPlayerServerId(target), requestedemote, otheremote)
+                    isRequestAnim = false
+                else
+                    SimpleNotify(Config.Languages[lang]['nobodyclose'])
                 end
-                if otheremote == nil then otheremote = requestedemote end
-                TriggerServerEvent("ServerValidEmote", GetPlayerServerId(target), requestedemote, otheremote)
+            elseif IsControlJustPressed(1, 182) and isRequestAnim then
+                SimpleNotify(Config.Languages[lang]['refuseemote'])
                 isRequestAnim = false
-            else
-                SimpleNotify(Config.Languages[lang]['nobodyclose'])
             end
-        elseif IsControlJustPressed(1, 182) and isRequestAnim then
-            SimpleNotify(Config.Languages[lang]['refuseemote'])
-            isRequestAnim = false
         end
-    end
-end)
-
+    end)
+end
 -----------------------------------------------------------------------------------------------------
 ------ Functions and stuff --------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------------
@@ -145,14 +147,14 @@ function GetClosestPlayer()
     local players = GetPlayers()
     local closestDistance = -1
     local closestPlayer = -1
-    local ply = PlayerPedId()
+    local ply = GetPlayerPed(-1)
     local plyCoords = GetEntityCoords(ply, 0)
 
     for index,value in ipairs(players) do
         local target = GetPlayerPed(value)
         if(target ~= ply) then
             local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
-            local distance = #(targetCoords - plyCoords)
+            local distance = GetDistanceBetweenCoords(targetCoords["x"], targetCoords["y"], targetCoords["z"], plyCoords["x"], plyCoords["y"], plyCoords["z"], true)
             if(closestDistance == -1 or closestDistance > distance) then
                 closestPlayer = value
                 closestDistance = distance
