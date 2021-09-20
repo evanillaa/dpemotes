82b6d6b25d02f986469c68caa2a778139cc1f310
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-20 11:15:08 +0300

    update dpemote with keybinds

--- Client/Ragdoll.lua
+++ Client/Ragdoll.lua
@@ -1,25 +1,37 @@
-local isInRagdoll = false
+local ragdoll = false
+function setRagdoll(flag)
+  ragdoll = flag
+  whileRag()
+end
 
-Citizen.CreateThread(function()
- while true do
-    Citizen.Wait(10)
-    if isInRagdoll then
-      SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, 0, 0, 0)
+function whileRag()
+  Citizen.CreateThread(function()
+    while ragdoll do
+      Citizen.Wait(0)
+      if ragdoll then
+        SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, 0, 0, 0)
+      end
     end
-  end
+  end)
+end
+
+ragdol = true
+RegisterNetEvent("Ragdoll")
+AddEventHandler("Ragdoll", function()
+	if ( ragdol ) then
+		setRagdoll(true)
+		ragdol = false
+	else
+		setRagdoll(false)
+		ragdol = true
+	end
 end)
 
-Citizen.CreateThread(function()
-    while true do
-    Citizen.Wait(0)
-    if IsControlJustPressed(2, Config.RagdollKeybind) and Config.RagdollEnabled and IsPedOnFoot(PlayerPedId()) then
-        if isInRagdoll then
-            isInRagdoll = false
-        else
-            isInRagdoll = true
-            Wait(500)
-        end
-    end
-  end
+RegisterCommand("rag", function(source, args, raw)
+    TriggerEvent("Ragdoll")
 end)
 
+RegisterKeyMapping('(Anim) Ragdoll', 'RagDoll', 'keyboard', Config.RagdollKeybind)
+RegisterCommand('RagDoll', function()
+  TriggerEvent("Ragdoll")
+end)
