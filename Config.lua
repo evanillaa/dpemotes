4888bf3afc2c0b2551e9694abc68dd84320e4a41
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-22 17:48:59 +0300

    finish edit

--- Config.lua
+++ Config.lua
@@ -1,101 +1,99 @@
 Config = {
-      MenuHead = 'The Rise Of Reflex',
-      -- Change the language of the menu here!.
-      -- Note fr and de are google translated, if you would like to help out with translation / just fix it for your server check below and change translations yourself
-      -- try en, fr, de or sv.
+	MenuHead = 'QBCore Emotes',
+	-- Change the language of the menu here!.
+	-- Note fr and de are google translated, if you would like to help out with translation / just fix it for your server check below and change translations yourself
+	-- try en, fr, de or sv.
 	MenuLanguage = 'en',	
-      -- Set this to true to enable some extra prints
+	-- Set this to true to enable some extra prints
 	DebugDisplay = false,
-      -- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
+	-- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
 	EnableXtoCancel = true,
-      -- Set this to true if you want to disarm the player when they play an emote.
+	-- Set this to true if you want to disarm the player when they play an emote.
 	DisarmPlayer= false,
-      -- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
-    AllowedInCars = true,
-      -- You can disable the (F9) menu here / change the keybind.
-	MenuKeybindEnabled = true,
+	-- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
+	AllowedInCars = true,
+	-- You can disable the (F9) menu here / change the keybind.
+	MenuKeybindEnabled = false,
 	MenuKeybind = 'F9', -- Get the button name here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-      -- You can disable the Favorite emote KeyBinding here.
+	-- You can disable the Favorite emote KeyBinding here.
 	FavKeybindEnabled = true,
 	FavKeybind = '', -- Get the button name here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-      -- You can change the header image for the f9 menu here
-      -- Use a 512 x 128 image!
-      -- NOte this might cause an issue of the image getting stuck on peoples screens
+	-- You can change the header image for the f9 menu here
+	-- Use a 512 x 128 image!
+	-- NOte this might cause an issue of the image getting stuck on peoples screens
 	CustomMenuEnabled = false,
 	MenuImage = "https://i.imgur.com/kgzvDwQ.png",
-      -- You can change the menu position here
+	-- You can change the menu position here
 	MenuPosition = "right", -- (left, right)
-      -- You can disable the Ragdoll KeyBinding here.
-	RagdollEnabled = true,
+	-- You can disable the Ragdoll KeyBinding here.
+	RagdollEnabled = false,
 	RagdollKeybind = '', -- Get the button number here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-      -- You can disable the Facial Expressions menu here.
+	-- You can disable the Facial Expressions menu here.
 	ExpressionsEnabled = true,
-      -- You can disable the Walking Styles menu here.
+	-- You can disable the Walking Styles menu here.
 	WalkingStylesEnabled = true,	
-      -- You can disable the Shared Emotes here.
-      SharedEmotesEnabled = true,
-      CheckForUpdates = false,
-      -- Keybinds
-      KeyBinding = true,
+	-- You can disable the Shared Emotes here.
+	SharedEmotesEnabled = true,
+	CheckForUpdates = false,
+	-- Keybinds
+	KeyBinding = true,
 }
 
-
 Config.Languages = {
-  ['en'] = {
-      ['emotes'] = 'Emotes',
-      ['danceemotes'] = "🕺 Dance Emotes",
-      ['propemotes'] = "📦 Prop Emotes",
-      ['favoriteemotes'] = "🌟 Favorite",
-      ['favoriteinfo'] = "Select an emote here to set it as your favorite.",
-      ['rfavorite'] = "Reset favorite",
-      ['prop2info'] = "❓ Prop Emotes can be located at the end",
-      ['set'] = "Set (",
-      ['setboundemote'] = ") to be your bound emote?",
-      ['newsetemote'] = "~w~ is now your bound emote, press ~g~CapsLock~w~ to use it.",
-      ['cancelemote'] = "Cancel Emote",
-      ['cancelemoteinfo'] = "~r~X~w~ Cancels the currently playing emote",
-      ['walkingstyles'] = "Walking Styles",
-      ['resetdef'] = "Reset to default",
-      ['normalreset'] = "Normal (Reset)",
-      ['moods'] = "Moods",
-      ['infoupdate'] = "Information",
-      ['infoupdateav'] = "Information (Update available)",
-      ['infoupdateavtext'] = "An update is available, get the latest version from ~y~https://github.com/andristum/dpemotes~w~",
-      ['suggestions'] = "Suggestions?",
-      ['suggestionsinfo'] = "'dullpear_dev' on FiveM forums for any feature/emote suggestions! ✉️",
-      ['notvaliddance'] = "is not a valid dance.",
-      ['notvalidemote'] = "is not a valid emote.",
-      ['nocancel'] = "No emote to cancel.",
-      ['maleonly'] = "This emote is male only, sorry!",
-      ['emotemenucmd'] = "Do /emotemenu for a menu.",
-      ['shareemotes'] = "👫 Shared Emotes",
-      ['shareemotesinfo'] = "Invite a nearby person to emote",
-      ['sharedanceemotes'] = "🕺 Shared Dances",
-      ['notvalidsharedemote'] = "is not a valid shared emote.",
-      ['sentrequestto'] = "Sent request to ~y~",
-      ['nobodyclose'] = "Nobody ~r~close~w~ enough.",
-      ['doyouwanna'] = "~y~Y~w~ to accept, ~r~L~w~ to refuse (~g~",
-      ['refuseemote'] = "Emote refused.",
-      ['makenearby'] = "makes the nearby player play",
-      ['camera'] = "Press ~y~G~w~ to use camera flash.",
-      ['makeitrain'] = "Press ~y~G~w~ to make it rain.",
-      ['pee'] = "Hold ~y~G~w~ to pee.",
-      ['spraychamp'] = "Hold ~y~G~w~ to spray champagne",
-      ['bound'] = "Bound ",
-      ['to'] = "to",
-      ['currentlyboundemotes'] = " Currently bound emotes:",
-      ['notvalidkey'] = "is not a valid key.",
-      ['keybinds'] = "🔢 Keybinds",
-      ['keybindsinfo'] = "Use",
-        -- added
-      ['nocommand'] = "No such command to unbind",
-      ['alreadyexist'] = "This command already exists",
-      ['invalidargs'] = "Invalid command arguments",
-      ['binds'] = "Binds",
-      ['nobinds'] = "You have no saved binds",
-      ['command'] = "Command",
-  },
-  ['fr'] = {
+	['en'] = {
+		['emotes'] = 'Emotes',
+		['danceemotes'] = "🕺 Dance Emotes",
+		['propemotes'] = "📦 Prop Emotes",
+		['favoriteemotes'] = "🌟 Favorite",
+		['favoriteinfo'] = "Select an emote here to set it as your favorite.",
+		['rfavorite'] = "Reset favorite",
+		['prop2info'] = "❓ Prop Emotes can be located at the end",
+		['set'] = "Set (",
+		['setboundemote'] = ") to be your bound emote?",
+		['newsetemote'] = "~w~ is now your bound emote, press ~g~CapsLock~w~ to use it.",
+		['cancelemote'] = "Cancel Emote",
+		['cancelemoteinfo'] = "~r~X~w~ Cancels the currently playing emote",
+		['walkingstyles'] = "Walking Styles",
+		['resetdef'] = "Reset to default",
+		['normalreset'] = "Normal (Reset)",
+		['moods'] = "Moods",
+		['infoupdate'] = "Information",
+		['infoupdateav'] = "Information (Update available)",
+		['infoupdateavtext'] = "An update is available, get the latest version from ~y~https://github.com/andristum/dpemotes~w~",
+		['suggestions'] = "Suggestions?",
+		['suggestionsinfo'] = "'dullpear_dev' on FiveM forums for any feature/emote suggestions! ✉️",
+		['notvaliddance'] = "is not a valid dance.",
+		['notvalidemote'] = "is not a valid emote.",
+		['nocancel'] = "No emote to cancel.",
+		['maleonly'] = "This emote is male only, sorry!",
+		['emotemenucmd'] = "Do /emotemenu for a menu.",
+		['shareemotes'] = "👫 Shared Emotes",
+		['shareemotesinfo'] = "Invite a nearby person to emote",
+		['sharedanceemotes'] = "🕺 Shared Dances",
+		['notvalidsharedemote'] = "is not a valid shared emote.",
+		['sentrequestto'] = "Sent request to ~y~",
+		['nobodyclose'] = "Nobody ~r~close~w~ enough.",
+		['doyouwanna'] = "~y~Y~w~ to accept, ~r~L~w~ to refuse (~g~",
+		['refuseemote'] = "Emote refused.",
+		['makenearby'] = "makes the nearby player play",
+		['camera'] = "Press ~y~G~w~ to use camera flash.",
+		['makeitrain'] = "Press ~y~G~w~ to make it rain.",
+		['pee'] = "Hold ~y~G~w~ to pee.",
+		['spraychamp'] = "Hold ~y~G~w~ to spray champagne",
+		['bound'] = "Bound ",
+		['to'] = "to",
+		['currentlyboundemotes'] = " Currently bound emotes:",
+		['notvalidkey'] = "is not a valid key.",
+		['keybinds'] = "🔢 Keybinds",
+		['keybindsinfo'] = "Use",
+		['nocommand'] = "No such command to unbind",
+		['alreadyexist'] = "This command already exists",
+		['invalidargs'] = "Invalid command arguments",
+		['binds'] = "Binds",
+		['nobinds'] = "You have no saved binds",
+		['command'] = "Command",
+	},
+	['fr'] = {
         ['emotes'] = 'Emotes',
         ['danceemotes'] = "🕺 Emotes de danse",
         ['propemotes'] = "📦 Em Prop Emotes",
@@ -148,8 +146,8 @@ Config.Languages = {
         ['binds'] = "Binds",
         ['nobinds'] = "You have no saved binds",
         ['command'] = "Command",
-  },
-  ['de'] = {
+	},
+	['de'] = {
         ['emotes'] = 'Emotes',
         ['danceemotes'] = "🕺 Tanz-Emotes",
         ['propemotes'] = "📦 Prop-Emotes",
@@ -202,8 +200,8 @@ Config.Languages = {
         ['binds'] = "Binds",
         ['nobinds'] = "You have no saved binds",
         ['command'] = "Command",
-  },
-  ['sv'] = {
+	},
+	['sv'] = {
         ['emotes'] = 'Emotes',
         ['danceemotes'] = "🕺 Dans Emotes",
         ['propemotes'] = "📦 Objekt Emotes",
@@ -256,8 +254,8 @@ Config.Languages = {
         ['binds'] = "Binds",
         ['nobinds'] = "You have no saved binds",
         ['command'] = "Command",
-  },
-  ['es'] = {
+	},
+	['es'] = {
         ['emotes'] = "Animaciones",
         ['danceemotes'] = "🕺 Bailes",
         ['propemotes'] = "📦 Objetos",
@@ -310,5 +308,5 @@ Config.Languages = {
         ['binds'] = "Binds",
         ['nobinds'] = "You have no saved binds",
         ['command'] = "Command",
-  }
+	}
 }
